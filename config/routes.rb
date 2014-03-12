2b29783cc12c65d17c0fbc2cb039f1b6ae5e2103
Ramon J. Long III <ramonjlongiii@gmail.com>
2014-03-12 00:43:57 -0400

    Adding a default route.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ JrubyOnRailsSampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'comments#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
