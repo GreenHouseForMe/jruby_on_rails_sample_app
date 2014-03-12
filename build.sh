094beee4fdb5e81e3160f13fa93e08b1021b92a1
Ramon J. Long III <ramonjlongiii@gmail.com>
2014-03-12 01:01:34 -0400

    Commit before build.

--- build.sh
+++ build.sh
@@ -6,27 +6,34 @@
 echo "=====[ Simple Builder ]====="
 
 # Perform a git add and commit:
-echo "-----> Adding changes to repo:"
+echo "-----> Adding changes to repo..."
 git add --all
+echo "       'git add -all' complete!"
 
-echo "-----> Commiting changes to repo:"
+echo "-----> Commiting changes to repo..."
 git commit -m "Commit before build."
+echo "       'git commit' complete!"
 
 # Precompile assets:
-echo "-----> Precompiling assets:"
+echo "-----> Precompiling assets..."
 rake assets:precompile
+echo "       'rake assets:precomile' complete!"
 
 # Run our test suite:
-# echo "-----> Running test suite:"
+# echo "-----> Running test suite..."
+# echo "       Test suite complete!"
 
 # Run warble:
-echo "-----> Building WAR with Warble:"
-warble execute war
+echo "-----> Building WAR with Warble..."
+warble executable war
+echo "       'warble executable war' complete!"
 
 # Git push (modifiy or comment out as necessary):
 # echo "-----> Pushing to origin on master branch:"
 # git push master
+# echo "       'git push master' complete!"
 
 # Deploy to Heroku:
 # echo "-----> Deploying to Heroku:"
 # git push heroku
+# echo "       'git push heroku' complete!"
